bd6228e08ac1d18577d5c6c95f74914221721b2b
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-12-10 22:17:50 +0100

    Elbow method to choose best K

--- explainable_knn.py
+++ explainable_knn.py
@@ -138,12 +138,15 @@ for k_value in range(1, range_of_K+1):
     print(f"Computing for k: {k_value}")
     predicted_labels = []
     error_rates = []
+    actual_labels = []
     model = KNN(k=k_value, batch_size=batch_size)
     for i in range(10):
         prediction = model.compute_knn(query=test_dataset[i][0])
         predicted_labels.append(prediction)
+        actual_labels.append(test_dataset[i][1])
 
-    error = model.compute_error_rate(predicted_labels, test_dataset[:][1])
+
+    error = model.compute_error_rate(predicted_labels, actual_labels)
     print(f"Error for k = {k_value} is: {error}\n")
     error_rates.append(error)
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,19 @@
-explanations
+for k_value in range(1, range_of_K+1):
+#     print(f"Computing for k: {k_value}")
+#     predicted_labels = []
+#     error_rates = []
+#     model = KNN(k=k_value, batch_size=batch_size)
+#     for i in range(10):
+#         prediction = model.compute_knn(query=test_dataset[i][0])
+#         predicted_labels.append(prediction)
+
+#     error = model.compute_error_rate(predicted_labels, test_dataset[:][1])
+#     print(f"Error for k = {k_value} is: {error}\n")
+#     error_rates.append(error)
+
+# # Visualize
+# plt.plot([value+1 for value in range(range_of_K)], error_rates)
+# plt.xlabel("K-values")
+# plt.ylabel("Error rates")
+# plt.title("K-values vs Error rate")
+# plt.show()
